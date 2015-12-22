02e273f4b6fa11462a7846516216cad2870800c3
Marcin Müller (Dev) <mam@vie.altova.com>
2015-12-22 11:41:39 +0100

    Setting error limit to 200

--- dqc_testsuite.py
+++ dqc_testsuite.py
@@ -1,11 +1,11 @@
 # Copyright 2015 Altova GmbH
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -72,8 +72,8 @@ def elem_val(elem):
     return val
 
 def parse_variation(variation_elem):
-    """Parses the <variation> element and returns a dict containing meta-information about the given variation."""
-
+    """Parses the <variation> element and returns a dict containing meta-information about the given variation.""" 
+    
     variation = {
         'id': attr_val(variation_elem,'id'),
     }
@@ -116,24 +116,24 @@ def parse_variation(variation_elem):
             variation['results'] = results
         else:
             logging.warning('Testcase file %s contains unknown <variation> child element <%s>',elem.document.uri,elem.local_name)
-
+  
     return variation
 
 def load_testcase(testcase_uri):
     """Loads the testcase file and returns a dict with the testcase meta-information."""
     logging.info('Loading testcase %s',testcase_uri)
-
+    
     # Load the testcase file
     instance, log = xml.Instance.create_from_url(testcase_uri)
     # Check for any fatal errors
     if not instance:
         raise ValidationError('\n'.join(error.text for error in log))
     testcase_elem = instance.document_element
-
+    
     testcase = {
         'uri': instance.uri,
-    }
-
+    }    
+        
     # Iterate over all <testcase> child elements
     variations = []
     variation_ids = set()
@@ -156,7 +156,7 @@ def load_testcase(testcase_uri):
             testcase['ruleMessage'] = elem_val(elem)
         elif elem.local_name == 'variation':
             variation = parse_variation(elem)
-            variations.append(variation)
+            variations.append(variation)            
             if variation['id'] in variation_ids:
                 logging.warning('Testcase file %s contains variations with duplicate id %s',testcase_uri,variation['id'])
         else:
@@ -168,7 +168,7 @@ def load_testcase(testcase_uri):
 def load_testsuite(index_uri):
     """Loads the testcases specified in the given testsuite index file and returns a dict with all testcase meta-information."""
     logging.info('Loading testsuite index %s',index_uri)
-
+    
     # Load the testcase index file
     instance, log = xml.Instance.create_from_url(index_uri)
     # Check for any fatal errors
@@ -180,8 +180,8 @@ def load_testsuite(index_uri):
         'uri': instance.uri,
         'name': attr_val(documentation_elem,'name'),
         'date': attr_val(documentation_elem,'date')
-    }
-
+    }    
+        
     # Iterate over all <testcase> child elements and parse the testcase file
     testcases = []
     for testcases_elem in documentation_elem.element_children():
@@ -194,9 +194,9 @@ def load_testsuite(index_uri):
                     # Load the testcase file
                     testcases.append(load_testcase(uri))
     testsuite['testcases'] = testcases
-
+            
     return testsuite
-
+    
 def instance_name_from_zip(path):
     """Determines the instance filename within a SEC EDGAR zip archive."""
     re_instance_name = re.compile(r'.+-\d{8}\.xml')
@@ -208,7 +208,7 @@ def instance_name_from_zip(path):
 def execute_variation(testcase,variation):
     """Peforms the actual XBRL instance or taxonomy validation and returns 'PASS' if the actual outcome is conformant with the result specified in the variation."""
     logging.info('[%s] Start executing variation',variation['id'])
-
+    
     if 'readMeFirst' in variation['data']:
         if variation['data']['readMeFirst'].endswith('.zip'):
             tmpzip = tempfile.NamedTemporaryFile(suffix='.zip',delete=False).name
@@ -221,7 +221,7 @@ def execute_variation(testcase,variation):
         raise RuntimeError('Unknown entry point in variation %s' % variation['id'])
 
     logging.info('[%s] Validating instance %s',variation['id'],uri)
-    instance, error_log = xbrl.Instance.create_from_url(uri)
+    instance, error_log = xbrl.Instance.create_from_url(uri,error_limit=200)
     dqc_validation.validate(instance,error_log,{'suppressErrors': variation['results']['blockedMessageCodes']})
     if error_log.has_errors() and logging.getLogger().isEnabledFor(logging.DEBUG):
         logging.debug('[%s] Error log:\n%s',variation['id'],'\n'.join(error.text for error in error_log))
@@ -247,10 +247,10 @@ def execute_testsuite(testsuite,args):
     """Runs all testcase variations in parallel and returns a dict with the results of each testcase variation."""
     logging.info('Start executing %s variations in %d testcases',sum(len(testcase['variations']) for testcase in testsuite['testcases']),len(testsuite['testcases']))
     start = time.time()
-
+  
     results = {}
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.max_workers) as executor:
-
+        
         # Schedule processing of all variations as futures
         futures = {}
         for testcase in testsuite['testcases']:
@@ -260,7 +260,7 @@ def execute_testsuite(testsuite,args):
                 if args.variation_ids and variation['id'] not in args.variation_ids:
                     continue
                 futures[executor.submit(execute_variation,testcase,variation)] = (testcase['uri'],variation['id'])
-
+        
         # Wait for all futures to finish
         for future in concurrent.futures.as_completed(futures):
             variation_key = futures[future]
@@ -294,7 +294,7 @@ def write_csv_report(path,testsuite,results,runtime,relative_uris):
             for variation in testcase['variations']:
                 variation_key = (testcase['uri'],variation['id'])
                 if variation_key in results:
-                    instance_uri = variation['data']['readMeFirst'] if not relative_uris else variation['data']['readMeFirst'][len(testsuite_path)+1:]
+                    instance_uri = variation['data']['readMeFirst'] if not relative_uris else variation['data']['readMeFirst'][len(testsuite_path)+1:]          
                     status, error_counts = results[variation_key]
                     actual = ' '.join('%dx%s'%(count,code) for code, count in sorted(error_counts.items()))
                     expected = ' '.join('%dx%s'%(error['count'],code) for code, error in sorted(variation['results']['errors'].items()))
@@ -314,11 +314,11 @@ def write_xml_report(path,testsuite,results,runtime,relative_uris):
     with open(path,'w') as xmlfile:
         testsuite_path, testsuite_index = testsuite['uri'].rsplit('/',1)
         testsuite_uri = testsuite['uri'] if not relative_uris else testsuite_index
-
+    
         xmlfile.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         xmlfile.write('<testsuite\n\txmlns="http://www.altova.com/testsuite/results"\n')
         if relative_uris:
-            xmlfile.write('\txml:base="{}/"\n'.format(testsuite_path))
+            xmlfile.write('\txml:base="{}/"\n'.format(testsuite_path))      
         xmlfile.write('\turi="{}"\n\tname="{}"\n\ttotal="{}"\n\tfailed="{}"\n\tconformance="{}"\n\truntime="{}"\n\texecution-date="{:%Y-%m-%dT%H:%M:%S}"\n\tprocessor="Altova RaptorXML+XBRL Server">\n'.format(testsuite_uri,testsuite['name'],total,failed,conformance,runtime,datetime.datetime.now()))
         for testcase in testsuite['testcases']:
             testcase_uri = testcase['uri'] if not relative_uris else testcase['uri'][len(testsuite_path)+1:]
@@ -342,7 +342,7 @@ def write_xml_report(path,testsuite,results,runtime,relative_uris):
         xmlfile.write('</testsuite>\n')
 
 def print_results(testsuite,results,runtime):
-    """Writes testsuite run summary to console."""
+    """Writes testsuite run summary to console."""    
     total,failed,conformance = calc_conformance(results)
     for testcase in testsuite['testcases']:
         for variation in testcase['variations']:
@@ -378,7 +378,8 @@ def run_xbrl_testsuite(uri,args):
 def setup_logging(args):
     """Initializes Python logging module."""
     if args.log_file:
-        logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',filename=args.log_file,filemode='w',level=logging.DEBUG if args.log_level == 'DEBUG' else logging.INFO)
+        levels = {'ERROR': logging.ERROR, 'WARNING': logging.WARNING, 'INFO': logging.INFO, 'DEBUG': logging.DEBUG}
+        logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',filename=args.log_file,filemode='w',level=levels[args.log_level])
     else:
         logging.getLogger().addHandler(logging.NullHandler())
     console = logging.StreamHandler()
@@ -391,7 +392,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Execute the XBRL US DQC conformance testsuite using Altova RaptorXML+XBRL')
     parser.add_argument('uri', metavar='INDEX', help='main testsuite index file')
     parser.add_argument('-l','--log', metavar='LOG_FILE', dest='log_file', help='log output file')
-    parser.add_argument('--log-level', metavar='LOG_LEVEL', dest='log_level', choices=['INFO','DEBUG'], default='INFO', help='log level (INFO|DEBUG)')
+    parser.add_argument('--log-level', metavar='LOG_LEVEL', dest='log_level', choices=['ERROR','WARNING','INFO','DEBUG'], default='INFO', help='log level (ERROR|WARNING|INFO|DEBUG)')
     parser.add_argument('--csv-report', metavar='CSV_FILE', dest='csv_file', help='write testsuite results to csv')
     parser.add_argument('--xml-report', metavar='XML_FILE', dest='xml_file', help='write testsuite results to xml')
     parser.add_argument('--relative-uris', dest='relative_uris', action='store_true', help='write testcase uris relative to testsuite index file')
@@ -399,14 +400,14 @@ def parse_args():
     parser.add_argument('-v','--variation', metavar='VARIATION_ID', dest='variation_ids', nargs='*', help='limit execution to only this variation id')
     parser.add_argument('-w','--workers', metavar='MAX_WORKERS', type=int, dest='max_workers', default=multiprocessing.cpu_count(), help='limit number of workers')
     return parser.parse_args()
-
+    
 def main():
     # Parse command line arguments
     args = parse_args()
-
+    
     # Setup logging
-    setup_logging(args)
-
+    setup_logging(args)    
+    
     # Run the testsuite
     run_xbrl_testsuite(args.uri,args)
 
